18aec2d3f29dfe27b5e30e5dd44a892571a29758
Asset34 <artemiy.kashintsev@gmail.com>
2017-11-26 22:18:26 +0300

    Add error token factory

---
+++ src/Parser/TokenFactories/ErrorFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathOptimizer.Parser.Tokens;
+using MathOptimizer.Parser.Predicates;
+using MathOptimizer.Parser.TokenFactories.GeneralPredicates;
+
+namespace MathOptimizer.Parser.TokenFactories
+{
+    class ErrorFactory
+    {
+        public static bool Check(Position pos)
+        {
+            return !(NumberFactory.Check(pos)       ||
+                     VariableFactory.Check(pos)     ||
+                     FunctionNameFactory.Check(pos) ||
+                     OperatorFactory.Check(pos)     ||
+                     LBracketFactory.Check(pos)     ||
+                     RBracketFactory.Check(pos));
+        }
+        public static IErrorToken TakeToken(Position pos)
+        {
+            if (Check(pos))
+            {
+                Position start = new Position(pos);
+
+                while (!pos.IsEnd && Check(pos))
+                {
+                    pos++;
+                }
+
+                return new ErrorToken(Position.MakeString(start, pos));
+            }
+            else
+            {
+                Exception ex = new Exception("Cannot take the token");
+
+                ex.Source = "ErrorFactory";
+                ex.Data.Add("Position", pos.Number);
+
+                throw ex;
+            }
+        }
+
+        /* Produced token */
+        private class ErrorToken : IErrorToken
+        {
+            public ErrorToken(string str)
+            {
+                this.value = str;
+            }
+            public void Accept(ITokenVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+            public override string ToString()
+            {
+                return value;
+            }
+
+            private readonly string value;
+        }
+
+        private ErrorFactory() { }
+    }
+}
