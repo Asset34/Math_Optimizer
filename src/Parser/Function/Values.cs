7e9091daa24244fff9d47d4fe8e5b5da13eae2bf
Asset34 <artemiy.kashintsev@gmail.com>
2017-11-30 02:17:11 +0300

    Add math expression tree

--- src/Parser/Function/Values.cs
+++ src/Parser/Function/Values.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using MathOptimizer.Parser.Function.Tree;
+using MathOptimizer.Parser.Func.Tree;
 
-namespace MathOptimizer.Parser.Function
+namespace MathOptimizer.Parser.Func
 {
     class Values
     {
