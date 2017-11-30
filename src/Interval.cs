53ac289f75390033194fa0335c01c06f7e5cd4eb
Asset34 <artemiy.kashintsev@gmail.com>
2017-11-30 04:42:52 +0300

    Add Interval class

---
+++ src/Interval.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOptimizer
+{
+    class Interval
+    {
+        public Interval(double leftBorder, double rightBorder)
+        {
+            LeftBorder = leftBorder;
+            RightBorder = rightBorder;
+
+            checkBorders();
+        }
+        public Interval()
+            : this(0, 0)
+        {
+        }
+        public override string ToString()
+        {
+            return String.Format("[{0} ; {1}]", LeftBorder, RightBorder);
+        }
+
+        public double LeftBorder { get; }
+        public double RightBorder { get; }
+        public double Length
+        {
+            get { return RightBorder - LeftBorder; }
+        }
+        public double Center
+        {
+            get { return (LeftBorder + RightBorder) / 2.0; }
+        }
+
+        private void checkBorders()
+        {
+            if (LeftBorder > RightBorder)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
